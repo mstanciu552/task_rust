f018ab57deb2693f7b647322e2252fda1512a93d
mstanciu552 <mstanciu552@gmail.com>
2021-08-31 15:24:25 +0300

    added delete and update function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use rusqlite::{params, Connection, Result};
+use rusqlite::{Connection, Result};
 
 mod task;
 
 use task::Task;
 
 // TODO Add today date
+// TODO Add UI
 
 fn main() -> Result<()> {
     let conn = Connection::open("task_rust.db")?;
@@ -19,30 +20,16 @@ fn main() -> Result<()> {
         [],
     )?;
 
-    let task = Task {
-        id: Default::default(),
-        desc: "Test1".to_string(),
-        due_date: "2021-08-30".to_string(),
-        created_at: "2021-08-30".to_string(),
-    };
+    let task = Task::new(
+        "Test1".to_string(),
+        "2222-01-01".to_string(),
+        "2222-01-01".to_string(),
+    );
 
-    conn.execute(
-        "insert into Task (desc, due_date, created_at) values (?1, ?2, ?3)",
-        params![task.desc, task.due_date, task.created_at],
-    )?;
-
-    let mut stmt = conn.prepare("select * from Task")?;
-    let tasks = stmt.query_map([], |row| {
-        Ok(Task {
-            id: row.get(0)?,
-            desc: row.get(1)?,
-            due_date: row.get(2)?,
-            created_at: row.get(3)?,
-        })
-    })?;
+    let tasks = task.get(&conn)?;
 
     for t in tasks {
-        println!("{:#?}", t.unwrap());
+        println!("{:#?}", t);
     }
 
     Ok(())
--- src/task.rs
+++ src/task.rs
@@ -1,7 +1,61 @@
+use rusqlite::{params, Connection, Result};
+
 #[derive(Debug)]
 pub struct Task {
     pub id: i32,
     pub desc: String,
     pub due_date: String,
     pub created_at: String,
+}
+
+impl Task {
+    pub fn new(desc: String, due_date: String, created_at: String) -> Self {
+        Self {
+            id: Default::default(),
+            desc,
+            due_date,
+            created_at,
+        }
+    }
+
+    pub fn insert(&self, conn: &Connection) -> Result<()> {
+        conn.execute(
+            "insert into Task (desc, due_date, created_at) values (?1, ?2, ?3)",
+            params![self.desc, self.due_date, self.created_at],
+        )?;
+        Ok(())
+    }
+
+    pub fn get(&self, conn: &Connection) -> Result<Vec<Self>> {
+        let mut stmt = conn.prepare("select * from Task")?;
+        let res = stmt.query_map([], |row| {
+            Ok(Self {
+                id: row.get(0)?,
+                desc: row.get(1)?,
+                due_date: row.get(2)?,
+                created_at: row.get(3)?,
+            })
+        })?;
+
+        let mut tasks: Vec<Self> = Vec::new();
+
+        for task in res {
+            tasks.push(task.unwrap());
+        }
+
+        Ok(tasks)
+    }
+
+    pub fn delete(conn: &Connection, id: i32) -> Result<()> {
+        conn.execute("delete from Task where id=?1", params![id])?;
+        Ok(())
+    }
+
+    pub fn update(conn: &Connection, id: i32, task: Self) -> Result<()> {
+        conn.execute(
+            "update Task set desc=?1, due_date=?2, created_at=?3 where id=?4",
+            params![task.desc, task.due_date, task.created_at, id],
+        )?;
+        Ok(())
+    }
 }
